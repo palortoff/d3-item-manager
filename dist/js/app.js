2eade2b68f97819441b6cfa4004a15d6ff6df0d5
palortoff <github@larsflitter.de>
2015-09-15 23:22:48 +0200

    Add dist for version 1.0.1

--- dist/js/app.js
+++ dist/js/app.js
@@ -379,7 +379,7 @@
     angular.module('d3-item-manager').directive('navBar', function () {
         return {
             restrict:'E',
-            templateUrl: '/directives/navbar/navbar.template.html',
+            templateUrl: 'directives/navbar/navbar.template.html',
             scope: {},
             controller: 'NavBarController',
             controllerAs: 'vm'
