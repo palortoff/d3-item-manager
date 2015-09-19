3136a85fda2f9ca6ff43c4040b9041788d2fa566
palortoff <github@larsflitter.de>
2015-09-19 16:25:03 +0200

    Prevent caching of jsons

--- app/services/itemLoader.service.js
+++ app/services/itemLoader.service.js
@@ -3,15 +3,12 @@
 
     angular.module('d3-item-manager').factory('loadItems', loadItems);
 
-    function loadItems($http){
-        return  function (section)
-        {
-            return $http.get('items/'+section+'.json').
+    function loadItems($http) {
+        return function(section) {
+            return $http.get('items/' + section + '.json?' + Date.now()).
                 then(function(result) {
                     return result.data;
                 });
         }
     }
-
-
 })();
