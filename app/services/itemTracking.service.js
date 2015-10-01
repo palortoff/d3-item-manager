3c4bb41756812e93aa4fedec0550518114e83dbe
palortoff <github@larsflitter.de>
2015-10-01 23:09:37 +0200

    Take over items from old data structure

--- app/services/itemTracking.service.js
+++ app/services/itemTracking.service.js
@@ -8,13 +8,14 @@
     function itemTracking($timeout) {
         var tracking;
         var notifyTimer;
+        var tracking2;
         return {
             load,
             save
         };
 
         function load() {
-            //update();
+            upgradeIfNecessary();
             tracking = JSON.parse(localStorage.getItem(key)) || {};
             return tracking;
         }
@@ -24,11 +25,40 @@
             localStorage.setItem(key, JSON.stringify(tracking));
         }
 
-        function notifySave(){
+        function notifySave() {
             $timeout.cancel(notifyTimer);
-            notifyTimer = $timeout(function(){
-            toastr.success('Items saved', {timeOut: 1000});
+            notifyTimer = $timeout(function() {
+                toastr.success('Items saved', {timeOut: 1000});
             }, 1000);
         }
+
+        function upgradeIfNecessary() {
+            upgradeFromCubeSectionsToOneTrackingContainer();
+        }
+
+        function upgradeFromCubeSectionsToOneTrackingContainer() {
+            var hasOldSectionData = !!localStorage.getItem('armor') || !!localStorage.getItem('weapons') || !!localStorage.getItem('jewelry');
+            var hasTrackingContainer = !!localStorage.getItem(key);
+
+            if (hasOldSectionData && !hasTrackingContainer) {
+                var armor = JSON.parse(localStorage.getItem('armor'));
+                var weapons = JSON.parse(localStorage.getItem('weapons'));
+                var jewls = JSON.parse(localStorage.getItem('jewelry'));
+                tracking = _.defaults({}, armor, weapons, jewls);
+                save();
+                console.log("upgradedFromCubeSectionsToOneTrackingContainer");
+
+                localStorage.setItem('armor_backup', JSON.stringify(armor));
+                localStorage.setItem('weapons_backup', JSON.stringify(weapons));
+                localStorage.setItem('jewls_backup', JSON.stringify(jewls));
+
+                localStorage.removeItem('armor');
+                localStorage.removeItem('weapons');
+                localStorage.removeItem('jewelry');
+
+            }
+
+        }
+
     }
 })();
