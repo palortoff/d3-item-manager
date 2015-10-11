543588f3428f5b8218ffb7bd2d2f889d376deaa2
palortoff <github@larsflitter.de>
2015-10-11 09:05:32 +0200

    Code cleanup, itemTracking part II

--- app/services/itemTracking/itemTracking.service.js
+++ app/services/itemTracking/itemTracking.service.js
@@ -5,7 +5,7 @@
 
     var key = 'itemTracking';
 
-    function itemTracking($timeout, itemCategory, upgradeDataStructureBeforeItemLoad) {
+    function itemTracking($timeout, upgradeDataStructureBeforeItemLoad, postItemLoadUpdate) {
         var tracking;
         var notifyTimer;
         return {
@@ -16,7 +16,7 @@
         function load() {
             upgradeDataStructureBeforeItemLoad();
             tracking = JSON.parse(localStorage.getItem(key)) || {};
-            removeDuplicateItemsIds();
+            postItemLoadUpdate();
             return tracking;
         }
 
@@ -35,53 +35,5 @@
                 toastr.success('Items saved', {timeOut: 1000});
             }, 1000);
         }
-
-        function removeDuplicateItemsIds() {
-            var hasBadData = _.chain(tracking)
-                    .filter(itemHasBadId)
-                    .filter(itemHasDataStored)
-                    .value()
-                    .length > 0;
-
-            if (hasBadData) {
-                notifyForBadData();
-                removeBadData();
-            }
-        }
-
-        function notifyForBadData() {
-            itemCategory.set(25);
-            toastr.error('You had items stored that conflicted with other items. Please check the displayed list of items.', 'Data loss detected', {timeOut: 0});
-        }
-
-        function removeBadData() {
-            _.forEach([1016,1026,1036,1046,1056,1066,1076,1086,1096,1106], function(id){delete tracking[id];});
-            saveWithoutToastr();
-        }
-
-        function itemHasBadId(item, key) {
-            switch (key) {
-                case "1016":
-                case "1026":
-                case "1036":
-                case "1046":
-                case "1056":
-                case "1066":
-                case "1076":
-                case "1086":
-                case "1096":
-                case "1106":
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
-        function itemHasDataStored(item) {
-            var s = item.hasOwnProperty('Softcore');
-            var h = item.hasOwnProperty('Hardcore');
-            return s || h;
-        }
-
     }
 })();
