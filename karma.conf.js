d01adf10acd871f99374c9da80af63b95ba90ab8
palortoff <github@larsflitter.de>
2015-10-13 22:21:58 +0200

    Add Unittests for item consistency

---
+++ karma.conf.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = function(config) {
+    //noinspection JSUnusedGlobalSymbols
+    config.set({
+        basePath:                 '',
+        frameworks:               ['jasmine', 'fixture'],
+        files:                    [
+            'bower_components/angular/angular.js',
+            'bower_components/underscore/underscore.js',
+            'app/**/*.js',
+            'tests/**/*.js',
+            {
+                pattern: 'items/*.json'
+            }
+        ],
+        singleRun:                true,
+        preprocessors:            {
+            "app/**/*.js": ['babel'],
+            "**/*.json":   ['json_fixtures']
+        },
+        browsers:                 ['Chrome_Mini'],
+        customLaunchers:          {
+            Chrome_Mini: {
+                base:  'Chrome',
+                flags: ['--window-size=400,200', '--window-position=-9999,0']
+            }
+        },
+        jsonFixturesPreprocessor: {
+            variableName: '__json__'
+        },
+        babelPreprocessor:        {
+            options:        {
+                sourceMap: 'inline'
+            },
+            filename:       function(file) {
+                return file.originalPath.replace(/\.js$/, '.es5.js');
+            },
+            sourceFileName: function(file) {
+                return file.originalPath;
+            }
+        }
+    });
+};
---
+++ tests/itemConsistency.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+describe('Item consistency', function() {
+    beforeAll(function() {
+        itemsEnGb = fixture.load(`items/items_en_GB.json`);
+    });
+    it(`there are no duplicate ids`, function() {
+
+        _.chain(itemsEnGb).
+            map(function(item) {return item.id;}).
+            forEach(expectOnlyOneItemWithId);
+
+        function expectOnlyOneItemWithId(id) {
+            let itemsWithId = _.filter(itemsEnGb, function(item) {return item.id === id;});
+            expect(itemsWithId.length).toEqual(1, `should have one item with id ${id}`);
+        }
+    });
+
+    describe('for all languages the ids and blizIds match', function() {
+        _.forEach(['de_DE', 'es_ES', 'fr_FR', 'it_IT', 'kr_KR','pl_PL', 'pt_PT', 'ru_RU', 'sh_TW'], function(lang) {
+            it(lang, function() {
+                let itemsInLang = fixture.load(`items/items_${lang}.json`);
+
+                _.forEach(itemsInLang, function(itemLang) {
+                    let itemEn = _.find(itemsEnGb, function(itemEn) {return itemEn.id === itemLang.id;});
+                    expect(itemEn).toBeDefined(`did not find id ${itemLang.id}`);
+                    expect(itemEn.blizId).toEqual(itemLang.blizId, `blizId mismatch for ${itemLang.id}`);
+                });
+            });
+        });
+
+    });
+
+    var itemsEnGb;
+});
